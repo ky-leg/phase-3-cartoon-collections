a231be644f77b09b97f52083cd6daa3ae8dc465d
Kyle Goodwin <kyle.goodwin93@gmail.com>
2022-02-21 23:48:59 -0500

    commit tree 3c45a88c667b0a81707a06dbb843a6b8e3085504

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,33 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves array
+  puts array.map.with_index {|n, i| "#{i+1}. #{n}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+# roll_call_dwarves ["Doc", "Dopey", "Bashful", "Grumpy"]
+
+def summon_captain_planet array
+  array.map {|c| "#{c.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls array
+  array.map do |w| 
+    if w.length > 4 
+    return true
+    end
+  end
+  false
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+
+def find_the_cheese strs
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.find do |cheese|
+    if strs.include?(cheese)
+      cheese
+    # else !strs.include?(cheese)
+    #   puts nil
+    end
+  end  
 end
+
+snacks = ["crackers", "cheddar cheese", "thyme"]
+find_the_cheese(snacks)
